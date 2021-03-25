7f2f10409df7e2c8d82a175ad8d60ea90b0abc9c
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-25 19:52:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
+  
+  static capitalize(String) {
+    return String.charAt(0).toUpperCase() + String.slice(1)
+  }
+  
+  static sanitize(String) {
+    
+  }
+  
+  static titleize(String) {
+    return Formatter.capitalize(string.split(" ").map(function(x) {
+      if (x != ("the" || "a" || "an" ||"but" ||  "for" || "at"|| "by" || "from" || "and" || "of"))
+      {
+        return Formatter.capitalize(x)
+      }
+      else
+      {
+        return x
+      }
+    }).join(" "))
+  };
+  }
 }
